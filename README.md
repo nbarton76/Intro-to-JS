f4beba187beca2626a4457aa674493488439970d
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-04-09 19:02:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intro-to-JS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-8vtgzx)
---
+++ script.js
@@ -0,0 +1,39 @@
+const addDevSkill = function (skill) {
+  let listItem = document.createElement('li');
+  listItem.innerHTML = skill;
+  document.getElementById("coding-section").appendChild(listItem);
+};
+
+const request = function (item) {
+return "I'd like a " + item + ', please!';
+};
+const drink = 'cocktail';
+let numberOfClicks = 0; 
+const clickHandler = function (text) {
+  numberOfClicks = numberOfClicks++;
+  console.log(numberOfClicks); 
+
+  if (numberOfClicks <= 3) {
+    alert(text);
+  } else {
+    alert('Drink in moderation -- no more cocktails for you!');
+  }
+}; 
+// Declare brainSkills array below this line
+const brainSkills = [ 
+"Front-End Architecture",
+"HTML",
+"CSS",
+"Command line operations" ,
+"Vim",
+"Git",
+"JavaScript Types",
+];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+   document.getElementById('cocktail-section').innerHTML = request(drink);
+   brainSkills.forEach(function (element) { 
+     addDevSkill(element);
+  });
+});
